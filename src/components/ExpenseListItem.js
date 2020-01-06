b8618f46b34bce27df69e6bb5adf3e8eb31ed1a0
Eric Owusu <llkoole7@gmail.com>
2020-01-05 20:44:49 -0800

    Add filter by dates

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,26 +1,20 @@
 import React from "react";
-import { connect } from "react-redux";
-import { removeExpense } from "../actions/expenses";
+import { Link } from "react-router-dom";
 
 const ExpenseListItem = props => {
-  const { description, amount, createdAt, id, dispatch } = props;
+  const { description, amount, createdAt, id } = props;
   return (
     <div>
       <li>
-        <h3>Description: {description}</h3>
+        <Link to={`/edit/${id}`}>
+          <h3>Description: {description}</h3>
+        </Link>
         <p>
           Amount: {amount} - {createdAt}
         </p>
-        <button
-          onClick={() => {
-            dispatch(removeExpense({ id }));
-          }}
-        >
-          Remove
-        </button>
       </li>
     </div>
   );
 };
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
